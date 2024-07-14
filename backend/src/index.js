547ea9b2cd5d0d48bbd1685f89a630ab91f137e1
Nikhil2005Rai <n2005rai@gmail.com>
2024-07-14 08:52:44 +0530

    addded basic app configurations

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,4 +6,17 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log(err);
+      throw err;
+    });
+    app.listen(process.env.port || 8000, () => {
+      console.log(`Server is listening on ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO DB connection failed: ", error);
+    process.exit(1);
+  });
