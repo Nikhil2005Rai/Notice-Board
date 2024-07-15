3d67d50c858dc7ea575b99a8fc7d31546a690521
Nikhil2005Rai <n2005rai@gmail.com>
2024-07-15 09:12:47 +0530

    Add fetch all notice comtroller function and its routing

--- backend/src/controllers/notice.controllers.js
+++ backend/src/controllers/notice.controllers.js
@@ -1,13 +1,17 @@
 import { asyncHandler } from "../../utils/asyncHandler.js";
 import { ApiError } from "../../utils/ApiError.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
-import { Notice } from './../models/notice.model.js';
+import { Notice } from "./../models/notice.model.js";
 //create notice
 const createNotice = asyncHandler(async (req, res) => {
   try {
-    const {title, description} = req.body;
+    const { title, description } = req.body;
 
-    const notice = await Notice.create({title, description, author: req.user._id})
+    const notice = await Notice.create({
+      title,
+      description,
+      author: req.user._id,
+    });
     const createdNotice = await Notice.findById(notice._id);
 
     if (!createdNotice) {
@@ -15,14 +19,28 @@ const createNotice = asyncHandler(async (req, res) => {
     }
 
     res
-    .status(201)
-    .json(
-        new ApiResponse(200, createdNotice, "Notice added successfully")
-    )
+      .status(201)
+      .json(new ApiResponse(200, createdNotice, "Notice added successfully"));
   } catch (error) {
-    throw new ApiError(500, "Server Error");
+    throw new ApiError(500, "Server Error while creating notice");
   }
 });
 
+//get all notices
+const getAllNotices = asyncHandler(async (_, res) => {
+  try {
+    const notices = await Notice.find().populate("author", "username");
 
-export { createNotice }
+    if (!notices) {
+      throw new ApiError(501, "Unable to fetch all notices");
+    }
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, notices, "All notices fetched successfully"));
+  } catch (error) {
+    throw new ApiError(500, "Server error while fetching all notices");
+  }
+});
+
+export { createNotice, getAllNotices };
--- backend/src/routes/notice.routes.js
+++ backend/src/routes/notice.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
-import { createNotice } from "../controllers/notice.controllers.js";
+import { createNotice, getAllNotices } from "../controllers/notice.controllers.js";
 import multer from "multer";
 
 const router = Router();
 const upload = multer();
 
 router.route("/create-notice").post(verifyJWT, upload.none(), createNotice);
+router.route("/all").get(verifyJWT, upload.none(), getAllNotices);
 
 export default router;
